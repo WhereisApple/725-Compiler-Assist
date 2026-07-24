e5ed17f1b15106ae3002fd3b0a3a81c6e41a3cfa
Your Name <your_email@example.com>
2026-07-24 22:48:49 +0530

    monaco editor updation

--- docs/js/editor.js
+++ docs/js/editor.js
@@ -17,7 +17,7 @@ function initEditor() {
 
     require.config({
         paths: {
-            vs: "https://cdn.jsdelivr.net/npm/monaco-editor@0.52.2/min/vs"
+            vs: "https://unpkg.com/monaco-editor@0.52.2/min/vs"
         }
     });
 
